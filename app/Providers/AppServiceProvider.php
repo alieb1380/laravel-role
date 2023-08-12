8e577e1394aacbe1f5982a6c8d3f426b0d7e7ecb
Ali Ebrahimi <alieb1380alieb@gmail.com>
2023-08-12 12:55:39 +0330

    testt

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,8 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env('REDIRECT_HTTPS')) {
-            URL::forceScheme('https');
-        }
+        Schema::defaultStringLength(125);
     }
 }
